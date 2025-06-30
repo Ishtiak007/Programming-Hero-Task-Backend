e5ba6ab811cef935452ca47aad08b5a54476be34
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-07-01 01:29:18 +0600

    listing validation added

---
+++ src/app/modules/Listings/listing.validation.ts
@@ -0,0 +1,83 @@
+import { z } from 'zod';
+
+const createListingValidationSchema = z.object({
+  body: z.object({
+    title: z.string().trim(),
+    description: z.string().trim(),
+    price: z.number(),
+    condition: z.enum(['new', 'likeNew', 'used', 'refurbished']),
+    images: z.array(z.string()),
+    userID: z.string().optional(),
+    status: z.enum(['available', 'sold']).default('sold'),
+    category: z.enum([
+      'home',
+      'electronics',
+      'books',
+      'furniture',
+      'tools',
+      'office',
+      'mobiles',
+      'vehicles',
+      'property',
+      'pets',
+      'cloths',
+      'sports',
+      'toys',
+      'beauty',
+      'fashion',
+      'music',
+      'gaming',
+      'groceries',
+      'baby',
+      'art',
+      'garden',
+      'jewelry',
+      'health',
+      'watches',
+      'travel',
+    ]),
+    brand: z.string().trim().optional(),
+    location: z.string().trim(),
+    negotiable: z.enum(['yes', 'no']).optional(),
+    warranty: z.string().optional(),
+    contactNumber: z.string().optional(),
+    isDeleted: z.boolean().default(false),
+  }),
+});
+
+const updateListingValidationSchema = z.object({
+  body: z.object({
+    title: z.string().trim().optional(),
+    description: z.string().trim().optional(),
+    price: z.number().optional(),
+    // condition: z.enum(['new', 'likeNew', 'used', 'refurbished']).optional(),
+    images: z.array(z.string()).optional(),
+    userID: z.string().optional(),
+    status: z.enum(['available', 'sold']).default('sold'),
+    category: z
+      .enum([
+        'wedding',
+        'birthday',
+        'corporate',
+        'concert',
+        'conference',
+        'festival',
+        'babyShower',
+        'engagement',
+        'anniversary',
+        'productLaunch',
+      ])
+      .optional(),
+    // brand: z.string().trim().optional(),
+    location: z.string().trim().optional(),
+    // negotiable: z.enum(['yes', 'no']).optional(),
+    // warranty: z.string().optional(),
+    contactNumber: z.string().optional(),
+    isDeleted: z.boolean().default(false),
+  }),
+});
+
+export const ListingValidationSchema = {
+  createListingValidationSchema,
+  updateListingValidationSchema,
+};
